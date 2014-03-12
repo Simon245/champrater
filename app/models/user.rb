279e690ee0d18268ccfbd16b150e81ae90e2d90b
simon245 <simonball245@gmail.com>
2014-03-12 18:00:52 +0800

    added email, password self authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,22 @@ class User
 
   attr_accessor :password, :password_confirmation
 
-  before_save :encrypt_password
-
   field :email, type: String
   field :salt, type: String
   field :fish, type: String
+
+  before_save :encrypt_password
+
+  def self.authenticate(email, password)
+    user = User.find_by email: email
+    user if user and user.authenticate(password)
+  end
+
+  def authenticate(password)
+    self.fish == BCrypt::Engine.hash_secret(password, self,salt)
+  end
+
+  protected
 
   def encrypt_password
     if password.present?
