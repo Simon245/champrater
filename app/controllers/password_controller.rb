1f7cd6767e34e98f01bb16f7c1b16bfefc80be49
simon245 <simonball245@gmail.com>
2014-03-20 15:43:15 +0800

    Fixed password controller using Anil's code

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -1,21 +1,21 @@
 class PasswordController < ApplicationController
 
   def edit
-    @user = User.find_by code: params[:code]
-    redirect_to_login_url, notice: "Your reset link has expired. Please generate a new one" unless @user
+    @user = User.find_by_code params[:code]
+    redirect_to login_url, notice: "Your reset link has expired, Please generate a new one" unless @user
   end
 
   def update
-    @user = User.find_by code: params[:code]
+    @user = User.find_by_code params[:code]
 
-    if @User
-      # if user is found
+    if @user
+    # if user is found
       if params[:user][:password].blank?
         @user.errors.add(:password, "This field can't be blank")
       elsif @user.update_attributes user_params
         render text: "Success!"
       else
-        render text: "Failure"
+        render text: "Failure!"
       end
 
     # otherwise show a message not found
@@ -30,5 +30,6 @@ class PasswordController < ApplicationController
   def user_params
     params.require(:user).permit(:password, :password_confirmation)
   end
+
 
 end
