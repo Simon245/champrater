6cad530a2c579afd5929bc3e3c26329e69acaa46
simon245 <simonball245@gmail.com>
2014-03-25 17:49:49 +0800

    Added some styling to move buttons

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -11,6 +11,20 @@ class CharactersController < ApplicationController
 
   end
 
-
+  def show
+    @images = ["Blitzcrank_0.jpg", "Caitlyn_0.jpg",  "Fiddlesticks_0.jpg",]
+    @random_no = rand(3)
+    @random_image = @images[@random_no]
+  end
 
 end
+
+
+
+      # "Jinx_0.jpg",
+      # "Miss Fortune_0.jpg",
+      # "Olaf_0.jpg",
+      # "Singed_0.jpg",
+      # "Tryndamere_0.jpg",
+      # "Vi_0.jpg",
+      # "Ziggs_0.jpg"]
