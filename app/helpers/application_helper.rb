b691971e1533c1e0ee4873af04e10472691df039
simon245 <simonball245@gmail.com>
2014-07-10 10:14:51 +0800

    Add return 0 to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
 
   def average_score
+    return 0 if @character.votes == 0
+
     return @character.total_score / @character.votes
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,19 @@
 # User.destroy_all
 
 User.create([
-    {email: "simonball245@gmail.com"},
-    {email: "simon245@gmail.com"}
+    {email: "simonball245@gmail.com", password: "123"},
+    {email: "simon245@gmail.com", password: "123"}
     ])
 
 Character.destroy_all
 
-Character.create({name: "Blitzcrank", image: "Blitzcrank_0.jpg", votes: 0 , score: 0})
-Character.create({name: "Caitlyn", image: "Caitlyn_0.jpg", votes: 0, score: 0})
-Character.create({name: "Fiddlesticks", image: "Fiddlesticks_0.jpg", votes: 0, score: 0})
-Character.create({name: "Jinx", image: "Jinx_0.jpg", votes: 0, score: 0})
-Character.create({name: "Miss Fortune", image: "Miss Fortune_0.jpg", votes: 0, score: 0})
-Character.create({name: "Olaf", image: "Olaf_0.jpg", votes: 0, score: 0})
-Character.create({name: "Singed", image: "Singed_0.jpg", votes: 0, score: 0})
-Character.create({name: "Tryndamere", image: "Tryndamere_0.jpg", votes: 0, score: 0})
-Character.create({name: "Vi", image: "Vi_0.jpg", votes: 0, score: 0})
-Character.create({name: "Ziggs", image: "Ziggs_0.jpg", votes: 0, score: 0})
+Character.create({name: "Blitzcrank", image: "Blitzcrank_0.jpg", votes: 0 , total_score: 0})
+Character.create({name: "Caitlyn", image: "Caitlyn_0.jpg", votes: 0, total_score: 0})
+Character.create({name: "Fiddlesticks", image: "Fiddlesticks_0.jpg", votes: 0, total_score: 0})
+Character.create({name: "Jinx", image: "Jinx_0.jpg", votes: 0, total_score: 0})
+Character.create({name: "Miss Fortune", image: "Miss Fortune_0.jpg", votes: 0, total_score: 0})
+Character.create({name: "Olaf", image: "Olaf_0.jpg", votes: 0, total_score: 0})
+Character.create({name: "Singed", image: "Singed_0.jpg", votes: 0, total_score: 0})
+Character.create({name: "Tryndamere", image: "Tryndamere_0.jpg", votes: 0, total_score: 0})
+Character.create({name: "Vi", image: "Vi_0.jpg", votes: 0, total_score: 0})
+Character.create({name: "Ziggs", image: "Ziggs_0.jpg", votes: 0, total_score: 0})
